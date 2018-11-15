b847b8d510e57f974fafa3b535107f70edb4bdc7
LachlanMarnham <lachlan.marnham@gmail.com>
2018-11-15 18:36:04 +0000

    Added main to quick_union.py

--- quick_union.py
+++ quick_union.py
@@ -24,3 +24,39 @@ class QuickUnion:
         id_p = self._root(p)
         id_q = self._root(q)
         self._id[id_p] = id_q
+
+
+if __name__ == "__main__":
+    """ 
+    A simple illustration. Consider the 3x3 grid (grid size of 9):
+
+        0 - 1   2
+            |
+        3 - 4 - 5
+
+        6 - 7 - 8
+
+    The following pairs of nearest neighbours share a nearest-neighbour connection:
+        (0, 1)
+        (1, 4)
+        (3, 4)
+        (4, 5)
+        (6, 7)
+        (7, 8)
+
+    """
+
+    grid_size = 9
+    grid = QuickUnion(grid_size)
+
+    # Register the pairs of nearest neighbours. See docstring.
+    grid.union(0, 1)
+    grid.union(1, 4)
+    grid.union(3, 4)
+    grid.union(4, 5)
+    grid.union(6, 7)
+    grid.union(7, 8)
+
+    print(grid.connected(0, 5))  # True; there is a path between 0 and 5
+    print(grid.connected(6, 8))  # True; there is a path between 6 and 8
+    print(grid.connected(1, 7))  # False; there is no path between 1 and 7
